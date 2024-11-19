2e38941a20a2d8b27842bdf663401b3f304bea42
Sujatha7981996839 <sujatha.nimeesha157@gmail.com>
2024-11-19 13:44:28 +0000

    solution

--- src/components/CashWithdrawal/index.js
+++ src/components/CashWithdrawal/index.js
@@ -2,118 +2,76 @@
 
 import {Component} from 'react'
 
-
-
 import DenominationItem from '../DenominationItem'
 
-
-
 import './index.css'
-
-
 
 // FIX3: class should be declared with keyword class
 
 class CashWithdrawal extends Component {
+  state = {
+    balance: 2000,
+  }
 
- state = {
+  updateBalance = value => {
+    // FIX4: setState spelling should be as mentioned below
 
-  balance: 2000,
+    this.setState(prevState => ({balance: prevState.balance - value}))
+  }
 
- }
+  render() {
+    // FIX5: The prop name here is denominationsList
 
+    const {denominationsList} = this.props
 
+    const {balance} = this.state
 
- updateBalance = value => {
+    const name = 'Sarah Williams'
 
-  // FIX4: setState spelling should be as mentioned below
+    const initial = name.slice(0, 1)
 
-  this.setState(prevState => ({balance: prevState.balance - value}))
+    return (
+      <div className="app-container">
+        <div className="cash-withdrawal-container">
+          <div className="user-details-container">
+            <div className="initial-container">
+              <p className="initial">{initial}</p>
+            </div>
 
- }
+            <p className="name">{name}</p>
+          </div>
 
+          <div className="balance-container">
+            <p className="your-balance">Your Balance</p>
 
+            <p className="balance">
+              {balance}
 
- render() {
+              <br />
 
-  // FIX5: The prop name here is denominationsList
+              <span className="currency">In Rupees</span>
+            </p>
+          </div>
 
-  const {denominationsList} = this.props
+          <p className="withdraw">Withdraw</p>
 
-  const {balance} = this.state
+          <p className="choose-sum">CHOOSE SUM (IN RUPEES)</p>
 
-  const name = 'Sarah Williams'
+          <ul className="denominations-list">
+            {/* FIX6: The prop name here is denominationsList */}
 
-  const initial = name.slice(0, 1)
-
-
-
-  return (
-
-   <div className="app-container">
-
-    <div className="cash-withdrawal-container">
-
-     <div className="user-details-container">
-
-      <div className="initial-container">
-
-       <p className="initial">{initial}</p>
-
+            {denominationsList.map(eachDenomination => (
+              <DenominationItem
+                key={eachDenomination.id}
+                denominationDetails={eachDenomination}
+                updateBalance={this.updateBalance}
+              />
+            ))}
+          </ul>
+        </div>
       </div>
-
-      <p className="name">{name}</p>
-
-     </div>
-
-     <div className="balance-container">
-
-      <p className="your-balance">Your Balance</p>
-
-      <p className="balance">
-
-       {balance}
-
-       <br />
-
-       <span className="currency">In Rupees</span>
-
-      </p>
-
-     </div>
-
-     <p className="withdraw">Withdraw</p>
-
-     <p className="choose-sum">CHOOSE SUM (IN RUPEES)</p>
-
-     <ul className="denominations-list">
-
-      {/* FIX6: The prop name here is denominationsList */}
-
-      {denominationsList.map(eachDenomination => (
-
-       <DenominationItem
-
-        key={eachDenomination.id}
-
-        denominationDetails={eachDenomination}
-
-        updateBalance={this.updateBalance}
-
-       />
-
-      ))}
-
-     </ul>
-
-    </div>
-
-   </div>
-
-  )
-
- }
-
+    )
+  }
 }
 
 export default CashWithdrawal
